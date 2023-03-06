c748debd5b13ba1b5ec601c74d35e96f0e9905ab
rhe <rhe8502@pm.me>
2023-03-06 22:16:34 +0100

    Update fake secret

--- fake_secret.py
+++ fake_secret.py
@@ -1,9 +1,24 @@
+import os
 
-def log_me_in():
-    apikey="ThisIsnotARealKey"
+# This script contains a secret that should not be committed to version control
+SECRET_KEY = "mysecretkey"
 
+# Use the secret key in your code
+def use_secret_key():
+    print(f"Using secret key: {SECRET_KEY}")
+
+# Get a secret from an environment variable
+def get_secret():
+    secret = os.getenv("MY_SECRET")
+    if secret is None:
+        print("Secret not found")
+    else:
+        print(f"Secret found: {secret}")
+
+# Main function that uses the secret
 def main():
-    log_me_in()
+    use_secret_key()
+    get_secret()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
